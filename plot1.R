98df916745f98beeb61ff671bc04ad8a5dc3cb74
NomiDee <naomi.herz@kcl.ac.uk>
2017-01-30 16:51:06 +0000

    Add files via upload

---
+++ plot1.R
@@ -0,0 +1,17 @@
+#Import full dataset
+dataFile<-"C:/Users/nh14/Dropbox (KiTEC)/Data Science - Coursera/data/household_power_consumption.txt"
+data<-read.table(dataFile, header=TRUE, sep=';', stringsAsFactors=FALSE, dec=".")
+
+#Subset the data
+data$Date=as.Date(data$Date,'%d/%m/%Y') 
+Feb2007 = subset(data, Date == '2007-02-01' | Date == '2007-02-02') 
+
+#convert global active power to numeric value
+Feb2007$Global_active_power=as.numeric(Feb2007$Global_active_power)
+
+#plot histogram
+hist(Feb2007$Global_active_power,col="red",xlab="Global Active Power (kilowatts)", ylab="Frequency",main="Global Active Power")
+
+#save to file
+dev.copy(png, file="plot1.png", width=480, height=480)
+dev.off()
